5e5abe4a939d482e73048bc652d608d6326b5ba7
Uboat46 <uboat46@hotmail.com>
2018-04-20 00:57:23 -0500

    modded main

--- src/main/java/mx/com/itam/adsi/ejercicio/Principal.java
+++ src/main/java/mx/com/itam/adsi/ejercicio/Principal.java
@@ -1,6 +1,10 @@
 package mx.com.itam.adsi.ejercicio;
 
 import org.apache.log4j.Logger;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Principal{
   private final static Logger LOG = Logger.getLogger(Principal.class);
@@ -9,9 +13,16 @@ public class Principal{
     Principal p = new Principal();
     LOG.info("Contar comentarios de System.out.print('Hello')// => Hello :" + p.cuenta("System.out.print('Hello')// => Hello"));
   }
-  public int[] cuenta(String source) {
+  public int[] cuenta(String source) throws FileNotFoundException, IOException { 
+        FileReader f = new FileReader(source);
+        BufferedReader b = new BufferedReader(f);
+        String line = null;
+        while(line = b.readLine()) {
+            LOG.info("Lineas :" + line);
+        }
+        b.close();
     int[] counter = {0, 0, 0};//[TOTAL, COMENTARIOS, EJECUTABLES]
-    int i = source.indexOf("//");
+    /*int i = source.indexOf("//");
 
     if(i >= 1){
       counter[1]++;
@@ -24,7 +35,7 @@ public class Principal{
     LOG.info("Cantidad de lineas :" + counter[0]);
     LOG.info("Cantidad de comments :" + counter[1]);
     LOG.info("Cantidad de exes :" + counter[2]);
-    
+    */
     return counter;
   }
 }
